155da5709ac3b90cfb04c7dd684de10dd149691e
Kresna Satya <kresnasatya72@gmail.com>
2025-07-13 22:35:36 +0800

    chore: remove melt-ui

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,3 @@
-import { preprocessMeltUI } from '@melt-ui/pp';
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -6,10 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [
-		vitePreprocess(),
-		preprocessMeltUI()
-	],
+	preprocess: vitePreprocess(),
 	kit: { adapter: adapter() }
 };
 
